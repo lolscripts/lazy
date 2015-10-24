8fc0c7ca05b9bc3310eba9bf379aeed0e4357933
DamnedN00b <singed1306@gmail.com>
2015-10-25 01:14:03 +0200

    v1.0.0.1
    improved E Logic, added basic Jungleclear

--- LazyLucianReworked/LazyLucian/Spells.cs
+++ LazyLucianReworked/LazyLucian/Spells.cs
@@ -190,23 +190,30 @@ namespace LazyLucian
 
         public static void CastEcombo()
         {
-            var target = TargetSelector.GetTarget(1200, DamageType.Magical);
+            var target = TargetSelector.GetTarget(1400, DamageType.Physical);
+            var vec = target.ServerPosition.Extend(ObjectManager.Player.Position,
+                target.ServerPosition.Distance(target.ServerPosition) + 400);
 
             if (target.Distance(ObjectManager.Player.ServerPosition) <=
                 500 && !Q.IsReady() &&
-                ((!W.IsReady() || W.GetPrediction(target).HitChance < HitChance.Medium)))
+                ((!W.IsReady() || W.GetPrediction(target).HitChance < HitChance.Medium)) &&
+                Helpers.IsSafePosition((Vector3) vec) && !vec.ToNavMeshCell().CollFlags.HasFlag(CollisionFlags.Wall))
             {
-                E.Cast(Game.CursorPos);
+                E.Cast((Vector3) vec);
             }
         }
 
         public static void CastEgap()
         {
-            var target = TargetSelector.GetTarget(1000 + E.Range, DamageType.Physical);
+            var target = TargetSelector.GetTarget(1400, DamageType.Physical);
+            var vec = ObjectManager.Player.ServerPosition.Extend(target, (E.Range));
 
-            if (target.Distance(ObjectManager.Player.ServerPosition) > 900) return;
+            if (target.Distance(ObjectManager.Player.ServerPosition) < (300 + E.Range) ||
+                target.Distance(ObjectManager.Player.Position) > (500 + E.Range) ||
+                (!Helpers.IsSafePosition((Vector3) vec) && !(target.HealthPercent <= 30)) ||
+                vec.ToNavMeshCell().CollFlags.HasFlag(CollisionFlags.Wall)) return;
             {
-                E.Cast((Vector3) ObjectManager.Player.ServerPosition.Extend(target.ServerPosition, E.Range));
+                E.Cast((Vector3) vec);
             }
         }
 
@@ -262,17 +269,17 @@ namespace LazyLucian
                         }
 
                         if (!bestChamp.ToNavMeshCell().CollFlags.HasFlag(CollisionFlags.Wall) &&
-                            (bestChamp.CountEnemiesInRange(800) <= 2))
+                            Helpers.IsSafePosition((Vector3) bestChamp))
                         {
                             E.Cast((Vector3) bestChamp);
                         }
                         else if (!bestMinion.ToNavMeshCell().CollFlags.HasFlag(CollisionFlags.Wall) &&
-                                 bestMinion.CountEnemiesInRange(800) <= 2)
+                                 Helpers.IsSafePosition((Vector3) bestMinion))
                         {
                             E.Cast((Vector3) bestMinion);
                         }
                         else if (!bestMonster.ToNavMeshCell().CollFlags.HasFlag(CollisionFlags.Wall) &&
-                                 bestMonster.CountEnemiesInRange(800) <= 2)
+                                 Helpers.IsSafePosition((Vector3) bestMonster))
                         {
                             E.Cast((Vector3) bestMonster);
                         }
